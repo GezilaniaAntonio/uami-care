91792a37804917f1b0e18c47776a4327dd64967c
Gezilania António <geizeantonioantonio@gmail.com>
2026-02-01 12:59:01 +0100

    first commit

---
+++ resources/views/layouts/_include/admin/navbar.blade.php
@@ -0,0 +1,230 @@
+  <!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+      <ul class="sidebar-nav" id="sidebar-nav">
+
+          <li class="nav-item">
+              <a class="nav-link " href="index.html">
+                  <i class="bi bi-grid"></i>
+                  <span>Dashboard</span>
+              </a>
+          </li><!-- End Dashboard Nav -->
+
+          <li class="nav-item">
+              <a class="nav-link " href="https://uamicare.ao/" target="_blank">
+                  <i class="bi bi-compass"></i>
+                  <span>Site</span>
+              </a>
+          </li><!-- End Dashboard Nav -->
+
+
+
+          <li class="nav-item">
+              <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                  <i class="bi bi-people"></i><span>Usuários</span><i class="bi bi-chevron-down ms-auto"></i>
+              </a>
+              <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                  <li>
+                      <a href="components-alerts.html">
+                          <i class="bi bi-circle"></i><span>Listar</span>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="components-accordion.html">
+                          <i class="bi bi-circle"></i><span>Cadastrar</span>
+                      </a>
+                  </li>
+              </ul>
+          </li><!-- End Components Nav -->
+
+
+          <li class="nav-item">
+              <a class="nav-link collapsed" data-bs-target="#plans-nav" data-bs-toggle="collapse" href="#">
+                  <i class="bi bi-menu-button-wide"></i><span>Planos</span><i class="bi bi-chevron-down ms-auto"></i>
+              </a>
+              <ul id="plans-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                  <li>
+                      <a href="components-alerts.html">
+                          <i class="bi bi-circle"></i><span>Listar</span>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="components-accordion.html">
+                          <i class="bi bi-circle"></i><span>Cadastrar</span>
+                      </a>
+                  </li>
+              </ul>
+          </li>
+
+          <li class="nav-item">
+              <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+                  <i class="bi bi-file-earmark-text"></i><span>Guia de Atendimento</span><i
+                      class="bi bi-chevron-down ms-auto"></i>
+              </a>
+              <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                  <li>
+                      <a href="forms-elements.html">
+                          <i class="bi bi-circle"></i><span>Listar</span>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="forms-layouts.html">
+                          <i class="bi bi-circle"></i><span>Cadastrar</span>
+                      </a>
+                  </li>
+
+              </ul>
+          </li><!-- End Forms Nav -->
+
+          <li class="nav-item">
+              <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
+                  <i class="bi bi-building"></i><span>Empresas</span><i class="bi bi-chevron-down ms-auto"></i>
+              </a>
+              <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                  <li>
+                      <a href="tables-general.html">
+                          <i class="bi bi-circle"></i><span>Listar</span>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="tables-data.html">
+                          <i class="bi bi-circle"></i><span>Cadastrar</span>
+                      </a>
+                  </li>
+              </ul>
+          </li><!-- End Tables Nav -->
+
+          <li class="nav-item">
+              <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
+                  <i class="bi bi-shield-check"></i><span>Seguradoras</span><i class="bi bi-chevron-down ms-auto"></i>
+              </a>
+              <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                  <li>
+                      <a href="charts-chartjs.html">
+                          <i class="bi bi-circle"></i><span>Listar</span>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="charts-apexcharts.html">
+                          <i class="bi bi-circle"></i><span>Cadastrar</span>
+                      </a>
+                  </li>
+
+              </ul>
+          </li><!-- End Charts Nav -->
+
+          <li class="nav-item">
+              <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
+                  <i class="bi bi-hospital"></i><span>Prestadores</span><i class="bi bi-chevron-down ms-auto"></i>
+              </a>
+              <ul id="icons-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                  <li>
+                      <a href="icons-bootstrap.html">
+                          <i class="bi bi-circle"></i><span>Listar</span>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="icons-remix.html">
+                          <i class="bi bi-circle"></i><span>Cadastrar</span>
+                      </a>
+                  </li>
+              </ul>
+          </li><!-- End Icons Nav -->
+
+          <li class="nav-item">
+              <a class="nav-link collapsed" data-bs-target="#pages-nav" data-bs-toggle="collapse" href="#">
+                  <i class="bi bi-person-badge"></i><span>Gestores</span><i class="bi bi-chevron-down ms-auto"></i>
+              </a>
+              <ul id="pages-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                  <li>
+                      <a href="pages-profile.html">
+                          <i class="bi bi-circle"></i><span>Listar</span>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="pages-faq.html">
+                          <i class="bi bi-circle"></i><span>Cadastrar</span>
+                      </a>
+                  </li>
+              </ul>
+          </li>
+
+          <li class="nav-item">
+              <a class="nav-link collapsed" data-bs-target="#reports-nav" data-bs-toggle="collapse" href="#">
+                  <i class="bi bi-people"></i><span>Agentes</span><i class="bi bi-chevron-down ms-auto"></i>
+              </a>
+              <ul id="reports-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                  <li>
+                      <a href="reports-sales.html">
+                          <i class="bi bi-circle"></i><span>Listar</span>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="reports-expenses.html">
+                          <i class="bi bi-circle"></i><span>Cadastrar</span>
+                      </a>
+                  </li>
+
+              </ul>
+          </li>
+
+          <li class="nav-item">
+              <a class="nav-link collapsed" data-bs-target="#clients-nav" data-bs-toggle="collapse" href="#">
+                  <i class="bi bi-telephone"></i><span>Call-Center</span><i class="bi bi-chevron-down ms-auto"></i>
+              </a>
+              <ul id="clients-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                  <li>
+                      <a href="clients-list.html">
+                          <i class="bi bi-circle"></i><span>Listar</span>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="clients-add.html">
+                          <i class="bi bi-circle"></i><span>Cadastrar</span>
+                      </a>
+                  </li>
+              </ul>
+          </li>
+
+          <li class="nav-item">
+              <a class="nav-link collapse" data-bs-target="#receipt-nav" data-bs-toggle="collapse" href="#">
+                  <i class="bi bi-receipt"></i><span>Facturação</span><i class="bi bi-chevron-down ms-auto"></i>
+              </a>
+              <ul id="receipt-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+                  <li>
+                      <a href="receipt-invoice.html">
+                          <i class="bi bi-circle"></i><span>Listar</span>
+                      </a>
+                  </li>
+                  <li>
+                      <a href="receipt-create.html">
+                          <i class="bi bi-circle"></i><span>Cadastrar</span>
+                      </a>
+                  </li>
+
+
+              </ul>
+          </li>
+
+          <li class="nav-heading">Páginas</li>
+
+          <li class="nav-item">
+              <a class="nav-link collapsed" href="users-profile.html">
+                  <i class="bi bi-gear"></i>
+                  <span>Configurações</span>
+              </a>
+          </li><!-- End Profile Page Nav -->
+
+
+          <li class="nav-item">
+              <a class="nav-link collapsed" href="pages-login.html">
+                  <i class="bi bi-box-arrow-in-left"></i>
+                  <span>Sair</span>
+              </a>
+          </li><!-- End Login Page Nav -->
+
+
+
+      </ul>
+
+  </aside><!-- End Sidebar-->
---
+++ resources/views/layouts/merge/dashboard.blade.php
@@ -0,0 +1,6 @@
+@include('layouts._include.admin.header')
+@include('layouts._include.admin.navbar')
+
+@yield('content')
+
+@include('layouts._include.admin.footer')
